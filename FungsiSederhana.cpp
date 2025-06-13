d15016b750c5000e3bfb173d1f13f96dd9a0e173
Diassaputraa <diassaputraa28@gmail.com>
2025-06-13 19:35:00 +0700

    Add files via upload

---
+++ FungsiSederhana.cpp
@@ -0,0 +1,18 @@
+#include <conio.h>
+#include <iostream>
+using namespace std;
+
+void garis() {
+    printf("\n-----------------------\n");
+}
+
+/* program utama */
+int main() {
+    garis(); //memanggil fungsi garis
+    cout << "DIAS UNPAM - Pamulang" << endl;
+    garis(); //memanggil
+    cout << "fungsi garis";
+    getche();
+    return 0;
+}
+
